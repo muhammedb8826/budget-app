092450416138095f023f2931810eea063ee045be
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-20 22:00:49 +0300

    Create a transaction page

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -3,4 +3,22 @@ class DealsController < ApplicationController
   def index
     @deals = Deal.all
   end
+  def new
+    @deal = Deal.new
+    @category = Category.find(params[:category_id])
+  end
+  def create
+    @category = Category.find(params[:category_id])
+    @deal = Deal.new(deal_params)
+    if @deal.save
+      redirect_to category_path(id: @category.id)
+    else
+      render :new
+    end
+  end
+
+  private
+  def deal_params
+    params.require(:deal).permit(:name, :amount, :author_id, category_ids: [])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "categories#index"
   resources :categories do
-     resources :transactions
+    resources :deals, only: [:new, :create]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
