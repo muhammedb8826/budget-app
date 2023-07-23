11c020ff798be84e3ff2933caf25fa4fa97e18d3
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-23 08:19:56 +0300

    Add integration test

---
+++ spec/integration/categories_index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Categories', type: :system do
+
+    let!(:user) { User.create name: 'Test User', email: 'test@123', password: '123456' }
+    let!(:category) { Category.create name: 'Test Category', icon: 'test', user_id: user.id }
+    let!(:deal) { Deal.create name: 'Test Deal', amount: 100, author_id: user.id, category_ids: [category.id] }
+
+    before do
+     sign_in user
+     visit categories_path
+    end
+
+    it 'shows the categories index page' do
+        expect(page).to have_content('Categories')
+    end
+end
